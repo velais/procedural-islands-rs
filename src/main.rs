19452ceee9f8a6de4b995dc654a7300b6d665ee7
Alex Cumming <velais@live.com>
2014-10-12 23:18:18 +1300

    mask

--- src/main.rs
+++ src/main.rs
@@ -67,7 +67,8 @@ impl World {
                 let value = if y < 5 || y > 195 || x < 5 || x > 195 {
                     -1.5f32
                 } else {
-                    rng.gen_range(-1.0, 1.0)
+                    //rng.gen_range(-1.0, 1.0)
+                    -0.5
                 };
                 self.map.push(value);
             }
@@ -90,6 +91,45 @@ impl World {
         let mut shuffle = result.as_mut_slice();
         task_rng().shuffle(shuffle);
         shuffle.into_vec()
+    }
+
+    pub fn rolling_particles(&mut self) {
+        let mut rng = task_rng();
+
+        let center_bias = false;
+        let edge_bias = 25f32;
+        let particle_length = 100u;
+
+        for iterations in range(0i, 3000) {
+            let (mut source_x, mut source_y) =  match center_bias {
+                true =>
+                    ((rng.gen_range(0.0, 1.0) * (self.width as f32 - (edge_bias * 2f32)) + edge_bias) as uint,
+                     (rng.gen_range(0.0, 1.0) * (self.height as f32 - (edge_bias * 2f32)) + edge_bias) as uint),
+                false =>
+                    ((rng.gen_range(0.0, 1.0) * self.width as f32 - 1f32) as uint,
+                     (rng.gen_range(0.0, 1.0) * self.height as f32 - 1f32) as uint)
+            };
+                    
+            for l in range(0, particle_length) {
+                source_x += ((rng.gen_range(0.0, 1.0) * 2f32 - 1f32).round()) as uint;
+                source_y += ((rng.gen_range(0.0, 1.0) * 2f32 - 1f32).round()) as uint;
+
+                if source_x < 1 || source_x > self.width as uint - 2 || source_y < 1 || source_y > self.height as uint - 2 {
+                    break;
+                }
+
+                let mut hood = self.get_neighbourhood(source_x as int, source_y as int);
+                for i in range(0u, hood.len()) {
+                    if self.sample(hood[i].x, hood[i].y) < self.sample(source_x as int, source_y as int) {
+                        source_x = hood[i].x as uint;
+                        source_y = hood[i].y as uint;
+                        break;
+                    }
+                }
+                let current = self.sample(source_x as int, source_y as int);
+                self.set_sample(source_x as int, source_y as int, current + 0.1f32);
+            }
+        }
     }
 
     fn sample(&mut self, x: int, y: int) -> f32 {
@@ -145,13 +185,10 @@ impl World {
 fn main() {
     let mut world = World::new(200i, 200i);
     world.generate(16i);
+    world.seed();
 
-    //png::save(&mut world);
-    let nb = world.get_neighbourhood(10, 10);
-    println!("{}", nb);
-
-
-    
+    let pm = world.rolling_particles();
+    png::save(&mut world);
 }
 
 
